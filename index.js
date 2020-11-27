ccd40a6015c845e54824fe2ea06c5a4871a1b79f
beyzanurgenc <beyzanurgenc@gmail.com>
2020-11-27 09:31:44 +0300

    updated for base64

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-function arrayBufferToWordArray(ab) {
-  var i8a = new Uint8Array(ab);
-  var a = [];
-  for (var i = 0; i < i8a.length; i += 4) {
-    a.push(i8a[i] << 24 | i8a[i + 1] << 16 | i8a[i + 2] << 8 | i8a[i + 3]);
-  }
-  return CryptoJS.lib.WordArray.create(a, i8a.length);
-}
 
 function handleFileSelect(evt) {
-  var files = evt.target.files; // FileList object
-
-  for (var i = 0, f; f = files[i]; i++) {
-    var reader = new FileReader();
-
-    reader.onloadend = (function(theFile) {
-      return function(e) {
-        var arrayBuffer = e.target.result;
-
-        var hash = CryptoJS.SHA256(arrayBuffer);
+  var f = evt.target.files[0]; // FileList object
+  var reader = new FileReader();
+  reader.onload = (function(theFile) {
+    return function(e) {
+      var binaryData = e.target.result;
+      //Converting Binary Data to base 64
+      var base64String = window.btoa(binaryData);
+	  console.log("base64String: " + base64String);
+      var hash = CryptoJS.SHA256(base64String);
         var elem = document.getElementById("hash");
         elem.value = hash;
-      };
-
-    })(f);
-    reader.onerror = function(e) {
-      console.error(e);
     };
-
-    reader.readAsArrayBuffer(f);
-  }
+  })(f);
+  reader.readAsBinaryString(f);
 }
 
 document.getElementById('document').addEventListener('change', handleFileSelect, false);
